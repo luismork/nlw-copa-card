d567bf7123ae74c32a5b0c9967071b99b0d2f6f1
luismork <luismork.tech@gmail.com>
2022-10-28 01:53:10 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+let isIgnite = true;
+const card = document.querySelector('.card');
+card.addEventListener('click', () => {
+    const bg = isIgnite ? "explorer" : "ignite";
+    isIgnite = !isIgnite;
+    card.style.backgroundImage = `url(./assets/bg-${bg}.svg)`;
+});
